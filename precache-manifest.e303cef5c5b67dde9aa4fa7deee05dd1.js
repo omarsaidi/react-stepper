1c97531f1e2a81aacfc26efda5c5a21134fd9723
Omar SAIDI <omar.saidi@mediawan.com>
2021-05-31 12:57:11 +0200

    Updates

---
+++ precache-manifest.e303cef5c5b67dde9aa4fa7deee05dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab554e6508f18e0777515d65cf0947eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae6cc4ea6117569dad34",
+    "url": "./static/css/main.f4196d20.chunk.css"
+  },
+  {
+    "revision": "1085dd9373cef70d1e40",
+    "url": "./static/js/2.e2e90157.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e2e90157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6cc4ea6117569dad34",
+    "url": "./static/js/main.9c7aea60.chunk.js"
+  },
+  {
+    "revision": "cac2c1d5dc35112e630d",
+    "url": "./static/js/runtime-main.af4eb86f.js"
+  }
+]);
